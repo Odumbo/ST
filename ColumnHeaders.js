270251fb130f9eb8d8c0b080b5a37d375a028203
Odumbo <99364810+Odumbo@users.noreply.github.com>
2022-06-26 19:12:57 -0700

    ColumnHeaders.js

---
+++ ColumnHeaders.js
@@ -0,0 +1,45 @@
+const ColumnHeaders = () => {
+    return (
+    <>
+<div class="box">
+        <div class="one">Name</div>
+        <div class="two">Price</div>
+        <div class="three">Seller</div>
+      </div>
+
+
+    {/* <item>Name</item>
+    <item>Price</item>
+    <item>Seller</item> */}
+    
+    </>
+    )
+}
+export default ColumnHeaders;
+
+
+
+// const ColumnHeaders = () => {
+//     return (
+//       <>
+//         <Grid item xs={4}>
+//           <Item>Name</Item>
+//         </Grid>
+//         <Grid item xs={4}>
+//           <Item>Price</Item>
+//         </Grid>
+//         <Grid item xs={4}>
+//           <Item>Seller</Item>
+//         </Grid>
+//       </>
+//     )
+//   }
+  
+//   export default ColumnHeaders;
+
+
+
+
+
+
+
