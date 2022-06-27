7c08c76e30479ebc44ebb5bcc5f969a98679e97f
Odumbo <99364810+Odumbo@users.noreply.github.com>
2022-06-26 19:19:09 -0700

    Index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
@@ -7,8 +7,31 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import "./index.css";
 import "font-awesome/css/font-awesome.css";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(<App/>,document.getElementById("root"));
+
+//ReactDOM.render(<App/>,document.getElementById("root"));
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(<App />);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
  
