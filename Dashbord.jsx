0c4a9d9aea013e710ad87d1f14cf560d6f4517fc
Odumbo <99364810+Odumbo@users.noreply.github.com>
2022-06-26 19:16:14 -0700

    Dashboard.jsx

--- Dashbord.jsx
+++ Dashbord.jsx
@@ -1,7 +1,55 @@
-import React, {Component} from "react";
-export default class Dashboard extends Component
-{
-    render(){
-    return (<div><h4>Dashboard</h4></div>); 
+import {useState, useEffect} from 'react';
+import ColumnHeaders from './ColumnHeaders';
+import NoMatchPage from './NoMatchPage';
+
+const Dashboard = () => {
+  const [posts, setPosts] = useState([])
+  useEffect (() => {
+    async function getPosts (){
+      const response = await fetch ('https://strangers-things.herokuapp.com/api/2202-ftb-pt-web-pt/posts')
+      const posts = await response.json();
+      console.log(posts);
+      setPosts (posts.data.posts);
+    }
+    getPosts();
+  }, [])
+
+return (
+  <main>
+    <h2>Posts</h2>
+    <table>
+      <tr>
+        <ColumnHeaders/>
+      </tr>
+      {/*<tr>
+      <td>Acer</td>
+      <td>$40</td>
+      <td>Min</td>
+      </tr>*/}
+    <div>
+      {posts.map(({_id, title, price, author}) => (
+        <NoMatchPage key={_id} id={_id} name={title} price={price} seller={author.username}/>
+      ))}
+    </div>
+    </table>
+  </main>
+)
 }
-}
+
+  
+export default Dashboard
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
