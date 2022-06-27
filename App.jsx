df00cf3795e48281a1e77442e1daf49a0eb31071
Odumbo <99364810+Odumbo@users.noreply.github.com>
2022-06-26 19:11:15 -0700

    App.jsx
    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
+import  ReactDOM  from 'react-dom/client';
 import NavBar from "./NavBar";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import ShoppingCart from "./ShoppingCart";
 import CustomersList from "./CustomersList";
-import { Route, Routes } from "react-router";
-import { BrowserRouter } from "react-router-dom";
+import Logout from "./Logout";
+//import { Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NoMatchPage from "./NoMatchPage";
+
 
 export default class App extends Component {
   render() {
@@ -19,6 +22,7 @@ export default class App extends Component {
             <Route path="/Dashboard" element={<Dashboard/>} />
             <Route path="/CustomersList" element={<CustomersList/>} />
             <Route path="/ShoppingCart" element={<ShoppingCart/>} />
+            <Route path="/Logout" element={<Logout/>} />
             <Route path="*" component={NoMatchPage} />
           </Routes>
         </div>
