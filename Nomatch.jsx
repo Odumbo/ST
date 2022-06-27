49a284a22bfad4c8222ee861f5ce47d100d016eb
Odumbo <99364810+Odumbo@users.noreply.github.com>
2022-06-26 19:22:47 -0700

    Nomatch.jsx

--- Nomatch.jsx
+++ Nomatch.jsx
@@ -1,11 +1,41 @@
-import React, { Component } from "react";
-
-export default class NoMatchPage extends Component {
-  render() {
-    return (
-      <div>
-        <h4>Page not found</h4>
-      </div>
-    );
-  }
+const NoMatchPage = ({name, price, seller})=> {
+  return(
+  <tr>
+  <td>{name}</td>
+  <td>{price}</td>
+  <td>{seller}</td>
+  
+  </tr>
+  )
 }
+
+export default NoMatchPage;
+
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+
+// export default class NoMatchPage extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h4>Page not found</h4>
+//       </div>
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
